21b11b73d880bcb95e37c96c296bf7b6a7646fc3
TheLooped <95268961+TheLooped@users.noreply.github.com>
2023-11-25 22:18:00 -0500

    Minor Polybar Changes, Todo: fix powermenu

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,14 +4,13 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
- polybar tray &
- polybar logo &
- polybar tags &
- polybar window &
- polybar layout &
- polybar date &
- polybar battery &
- polybar utils &
 
-    2>&1 | tee -a /tmp/polybar1.log & disown
-
+  polybar powmenu &
+  polybar logo &
+  polybar tags &
+  polybar window &
+  polybar layout &
+  polybar date &
+  polybar battery &
+  polybar utils &
+2>&1 | tee -a /tmp/polybar1.log & disown
